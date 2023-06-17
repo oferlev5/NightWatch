b8d8cd120a216219466fbd41d03d8e83d98ba0f0
Ofer Lev <oferlev5@outlook.co.il>
2023-06-17 14:41:48 +0300

    finished general look of dashboard

--- app/src/main/java/com/example/tutorial6/ui/home/FunctionalityFragment.java
+++ app/src/main/java/com/example/tutorial6/ui/home/FunctionalityFragment.java
@@ -2,6 +2,7 @@ package com.example.tutorial6.ui.home;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -21,35 +22,20 @@ import com.example.tutorial6.databinding.FragmentHomeBinding;
  */
 public class FunctionalityFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    CardView cardHome;
     private FragmentFunctionalityBinding binding;
 
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public FunctionalityFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FunctionalityFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static FunctionalityFragment newInstance(String param1, String param2) {
         FunctionalityFragment fragment = new FunctionalityFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -58,8 +44,8 @@ public class FunctionalityFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -69,6 +55,7 @@ public class FunctionalityFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentFunctionalityBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        cardHome = binding.cardHome;
 
         return root;
     }
